2f75bf39d854ca6226d559684fb193263344e595
nick lee <nicklee1006@gmail.com>
2019-11-24 14:53:39 +1000

    Added eureka client - service provider and consumer

---
+++ eureka/service-hello/src/main/java/springclouddemo/helloservice/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package springclouddemo.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
+}
