163921cb5860b20d5396d66e11026e84254ada25
nick lee <nicklee1006@gmail.com>
2019-12-12 14:14:18 +1000

    Added config server part 2

--- service-discovery/src/main/java/springclouddemo/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/springclouddemo/servicediscovery/ServiceDiscoveryApplication.java
@@ -3,10 +3,12 @@ package springclouddemo.servicediscovery;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceDiscoveryApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ServiceDiscoveryApplication.class).web(WebApplicationType.SERVLET).run(args);
